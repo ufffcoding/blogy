a62c3dd891a8cca69cb658acdaae99df14b6548b
Deepanshu Verma <ufff.coding@gmail.com>
2024-10-09 01:21:03 +0530

    update blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
 
-const BlogCard = ({ blog }) => {
-    return (
-        <Link to={`/blog/${blog?.$id}`}>
-            <div className="rounded-lg shadow-md overflow-hidden bg-white dark:bg-gray-800 transition-colors duration-300">
-                <img loading='lazy' src={blog?.imageUrl} alt={blog?.title} className="w-full h-64 object-cover" />
-                <div className="flex flex-col h-1/4 gap-2 p-4">
-                    <h3 className="text-lg font-bold text-gray-900 line-clamp-1 dark:text-white">{blog?.title}</h3>
-                    <div className="line-clamp-2 text-gray-600 dark:text-gray-300">{parse(`${blog?.description}`)}</div>
-                    <p className='text-xs dark:bg-gray-400 py-1 px-2 w-fit rounded text-gray-600 dark:text-white'>{new Date(blog?.$createdAt).toLocaleDateString()}</p>
-                </div>
+const BlogCard = ({ blog }) => (
+    <Link to={`/blog/${blog?.$id}`}>
+        <div className="relative max-w-lg rounded overflow-hidden">
+            <img className="w-full h-64 object-cover" loading='lazy' src={blog?.imageUrl} alt={blog?.title} />
+            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+            <div className="absolute bottom-0 left-0 p-6 text-white">
+                <h2 className="text-2xl line-clamp-1 font-bold mb-2">{blog?.title}</h2>
+                <p className="line-clamp-2 mb-4">{parse(`${blog?.description}`)}</p>
+                {/* <div className="flex justify-between items-center">
+                    <span>{blog?.author}</span>
+                    <span>{new Date(blog?.$createdAt).toLocaleDateString()}</span>
+                </div> */}
             </div>
-        </Link>
-    )
-};
+        </div>
+    </Link>
+);
 
-export default BlogCard;
+export default memo(BlogCard);
